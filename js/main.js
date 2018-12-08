2951052b33806ffd8e146195082fbb293b7e58b5
Jonathan Odlum <jonathan.odlum@gmail.com>
2018-12-09 09:31:42 +1100

    changed the page to the portfolio page

--- js/main.js
+++ js/main.js
@@ -1,183 +1,19 @@
-const insultGenerator = {
-  firstAdjectives: [
-    'artless',
-    'bawdy',
-    'beslubbering',
-    'bootless',
-    'churlish',
-    'cockered',
-    'clouted',
-    'craven',
-    'currish',
-    'dankish',
-    'dissembling',
-    'droning',
-    'errant',
-    'fawning',
-    'fobbing',
-    'froward',
-    'frothy',
-    'gleeking',
-    'goatish',
-    'gorbellied',
-    'impertinent',
-    'infectious',
-    'jarring',
-    'loggerheaded',
-    'lumpish',
-    'mammering',
-    'mangled',
-    'mewling',
-    'paunchy',
-    'pribbling',
-    'puking',
-    'puny',
-    'qualling',
-    'rank',
-    'reeky',
-    'roguish',
-    'ruttish',
-    'saucy',
-    'spleeny',
-    'spongy',
-    'surly',
-    'tottering',
-    'unmuzzled',
-    'vain',
-    'venomed',
-    'villainous',
-    'warped',
-    'wayward',
-    'weedy',
-    'yeasty'
-  ],
-
-  secondAdjectives: [
-    'base-court',
-    'bat-fowling',
-    'beef-witted',
-    'beetle-headed',
-    'boil-brained',
-    'clapper-clawed',
-    'clay-brained',
-    'common-kissing',
-    'crook-pated',
-    'dismal-dreaming',
-    'dizzy-eyed',
-    'doghearted',
-    'dread-bolted',
-    'earth-vexing',
-    'elf-skinned',
-    'fat-kidneyed',
-    'fen-sucked',
-    'flap-mouthed',
-    'fly-bitten',
-    'folly-fallen',
-    'fool-born',
-    'full-gorged',
-    'guts-griping',
-    'half-faced',
-    'hasty-witted',
-    'hedge-born',
-    'hell-hated',
-    'idle-headed',
-    'ill-breeding',
-    'ill-nurtured',
-    'knotty-pated',
-    'milk-livered',
-    'motley-minded',
-    'onion-eyed',
-    'plume-plucked',
-    'pottle-deep',
-    'pox-marked',
-    'reeling-ripe',
-    'rough-hewn',
-    'rude-growing',
-    'rump-fed',
-    'shard-borne',
-    'sheep-biting',
-    'spur-galled',
-    'swag-bellied',
-    'tardy-gaited',
-    'tickle-brained',
-    'toad-spotted',
-    'unchin-snouted',
-    'weather-bitten'
-  ],
-
-  nouns: [
-    'apple-john',
-    'baggage',
-    'barnacle',
-    'bladder',
-    'boar-pig',
-    'bugbear',
-    'bum-bailey',
-    'canker-blossom',
-    'clack-dish',
-    'clotpole',
-    'coxcomb',
-    'codpiece',
-    'death-token',
-    'dewberry',
-    'flap-dragon',
-    'flax-wench',
-    'flirt-gill',
-    'foot-licker',
-    'fustilarian',
-    'giglet',
-    'gudgeon',
-    'haggard',
-    'harpy',
-    'hedge-pig',
-    'horn-beast',
-    'hugger-mugger',
-    'joithead',
-    'lewdster',
-    'lout',
-    'maggot-pie',
-    'malt-worm',
-    'mammet',
-    'measle',
-    'minnow',
-    'miscreant',
-    'moldwarp',
-    'mumble-news',
-    'nut-hook',
-    'pigeon-egg',
-    'pignut',
-    'puttock',
-    'pumpion',
-    'ratsbane',
-    'scut',
-    'skainsmate',
-    'strumpet',
-    'varlot',
-    'vassal',
-    'whey-face',
-    'wagtail'
-  ],
-
-  genRandInt: function( range ){
-    return Math.floor(Math.random()*range);
-  },
-
-  pickWord: function( array ) {
-    let location = this.genRandInt(array.length);
-    return array[location];
-  },
-
-  makeInsult: function(){
-    const firstWord = this.pickWord(this.firstAdjectives);
-    const secondWord = this.pickWord(this.secondAdjectives);
-    const thirdWord = this.pickWord(this.nouns);
-    return `${firstWord} ${secondWord} ${thirdWord}`
-  },
-
+function setup(){
+  createCanvas(windowWidth, windowHeight);
+  background(0);
+  noFill();
+  noStroke();
+  colorMode(HSB, 255);
 };
 
-console.log(`Thou Art a ${insultGenerator.makeInsult()}!`);
+function draw(){
+  const x = random(windowWidth);
+  const y = random(windowHeight);
+  const size = 75;
+  const hue = map(x, 0, windowWidth, 0, 255);
+  const saturation = map(y, 0, windowHeight, 0, 255);
+  const brightness = map(x+y, 0, windowWidth+windowHeight, 0, 255);
 
-const again = function(){
-  console.log(`Thou Art a ${insultGenerator.makeInsult()}!`);
-}
+  fill( hue , saturation , brightness, 10)
+  ellipse(x, y, size, size);
+};
