467e2c4c1790afcc3332b08d6b2521480246f614
Jonathan Odlum <jonathan.odlum@gmail.com>
2018-12-11 10:56:07 +1100

    resized for consistancy

--- js/main.js
+++ js/main.js
@@ -7,13 +7,17 @@ function setup(){
 };
 
 function draw(){
-  const x = random(windowWidth);
-  const y = random(windowHeight);
-  const size = 75;
-  const hue = map(x, 0, windowWidth, 0, 255);
-  const saturation = map(y, 0, windowHeight, 0, 255);
-  const brightness = map(x+y, 0, windowWidth+windowHeight, 0, 255);
+  let i = 0;
+  while ( i < 10 ){
+    const x = random(windowWidth);
+    const y = random(windowHeight);
+    const size = 75;
+    const hue = map(x, 0, windowWidth, 0, 255);
+    const saturation = map(y, 0, windowHeight, 0, 255);
+    const brightness = map(x+y, 0, windowWidth+windowHeight, 0, 255);
 
-  fill( hue , saturation , brightness, 10)
-  ellipse(x, y, size, size);
+    fill( hue , saturation , brightness, 10)
+    ellipse(x, y, size, size);
+    i++
+  }
 };
